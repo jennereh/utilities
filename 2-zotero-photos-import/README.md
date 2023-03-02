581a722c440958dba4b270b419f24fc77f7ade43
jennereh <jenner@jennereh.com>
2023-03-01 18:18:12 -0800

    [2-zpi] Release script

--- 2-zotero-photos-import/README.md
+++ 2-zotero-photos-import/README.md
@@ -1,22 +1,49 @@
-# Zotero photo batch import
+# Zotero Photo Batch Import
 
 I use Obsidian and Zotero for my image management. This script allows the automatic import of individual files as well as folders of photo collections. 
+
+## Usage
+
+I should be able to run it from the terminal with the alias `zot` without having to navigate to any directories.
+
+All folders and files to process should be in `~/tosync/photos-to-process`.
+
+Each folder should be named in the form `mynameYEARMMDDalbumtitle`.
+
+The name of the contained files doesn't matter at all. They'll be processed in alphanumerical order. 
 
 ## Procedure
 
 ### Preparation
 
-1. Move individual files into the `~/tosync/photos-to-process` base folder.
-1. Rename individual files to 'nameYYYYMMDDthreewordtitle' format where the MMDD are optional. 
-1. For photo albums, manually create a folder following that naming scheme and move filenames into it in album order. 
+1. For collections of photos, manually create a folder following the naming scheme and move filenames into it in album order. 
+1. In each folder, include an `albuminfo` file for previous and next links, and a title for Zotero's artwork item title:
+
+```
+prev: 2004-01-09
+next: 2004-01-12
+title: Zotero Photo Item  Title
+```
 
 ### Script
 
-1. Run the script from any folder, it will `cd ~/tosync/photos-to-process`. 
-1. The script identifies and creates Zotero RDF entries for individual files in the base folder.
-1. The script renames every file in a folder to the name of that folder with a three-digit integer, i.e. `nameYYYYMMDDthreewordtitle001`.
-1. Manually batch import folders of existing photos image files by creating a Zotero RDF import to preserve the 
-attachment links. [link](2-zotero-photos-import)
+1. Run the script from any folder, it will use the default folder `~/tosync/photos-to-process`. 
+1. The script identifies and creates Zotero .bibfile entries for individual files in the base folder.
+1. The script renames every file in a folder to the name of that folder with a three-digit integer, i.e. `nameYYYYMMDDthreewordtitle001` in alphanumerical order.
+1. The script creates a markdown file of the form `YYYY-MM-DD` for use in Obsidian. If albuminfo or one of the params in albuminfo is missing, the script autofills with the previous day and next day based on the current entry's day. 
+1. The script copies the photo files into `~/files/photos`.
 
-given a base folder,
+### Manual Zotero Import
+
+1. Manually batch import folders of existing photos image files by uploading the Zotero .bibfile.
+1. The type needs to be set to Artwork manually. Copy the object string below to the clipboard and select all of the items. Right click and select `Paste item type`. 
+1. When the items are all showing up as Artwork, right click again and select `Paste into empty item fields`.
+
+```
+{
+  "itemType": "artwork",
+  "language": "en",
+  "medium": "Photograph"
+}
+```
 
--- 2-zotero-photos-import/zoteroPhotosImport.py
+++ 2-zotero-photos-import/zoteroPhotosImport.py
@@ -1,22 +1,148 @@
 # zoterOPhotosImport.py
 
-import os
+import os, glob, re
+from datetime import datetime
 
 base_folder = "/home/jenner/tosync/photos-to-process"
+dest_folder = "/home/jenner/files"
+obsi_folder = "/home/jenner/obsidian"
+temp_folder = "/home/jenner/tosync"
+
+weekdays = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
+months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", 
+           "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+
+def parseCitekey(strToParse):
+  regex = re.compile(r'(?P<name>[a-zA-Z]+)(?P<year>[0-9]+)(?P<title>\w+)')
+  res = regex.search(strToParse)
+  return res
 
 print("zoteroPhotosImport.py")
 
 # if the folder does not exist, exit with an error
-
+# if it does exist, change directory 
 if not os.path.isdir(base_folder):
   print(f"{base_folder} does not exist.\nexiting...")
+  input("Press [Enter] to continue.") 
   exit()
+else: 
+  os.chdir(base_folder)
 
-# glob the individual files in base_folder
-# for each file,
-#   
 # glob the folders in base_folder
-# for each folder, 
-#   get the citekey
-#   parse the citekey
-#   rename all the contained files
+foldersList = glob.glob('*', recursive=True)
+markdown_file = []
+bibfile_entries = []
+mdPrevStr = ''
+mdNextStr = ''
+mdTitleStr = ''
+
+# for every folder in the base_folder,
+# first change to that folder
+for f in foldersList:
+  if f == 'albuminfo':
+    continue
+
+  os.chdir(base_folder+'/'+f)
+  
+  # read in 'albuminfo'
+  if not os.path.exists('albuminfo'):
+    print(f, "albuminfo doesn't exist")
+
+  else:
+    with open('albuminfo') as albumInfoFile:
+      for line in albumInfoFile:
+        if 'prev: ' in line:
+            mdPrevStr = line.lstrip('prev: ').rstrip('\n')
+        if 'next: ' in line:
+            mdNextStr = line.lstrip('next: ').rstrip('\n')
+        if 'title: ' in line:
+            mdTitleStr = line.lstrip('title: ').rstrip('\n')
+
+  # build the date string from the filename
+  parsedDate = parseCitekey(f)['year']
+  parsedDate = datetime.strptime(parsedDate, '%Y%m%d')
+  mdFileDate = parsedDate.strftime('%Y-%m-%d')
+
+  # get the markdown file full path so you can create it
+  mdFilePath = obsi_folder+'/journal/'+mdFileDate+'.md'
+
+  # get the markdown file title
+  # assign the abbreviated var for readabiity
+  pD = parsedDate
+  mdFileTitle = f"# {weekdays[pD.weekday()]} {pD.day} {months[pD.month-1]} {pD.year}"
+
+  # build the markdown file nav string
+  mdFileNav = f"[[{mdPrevStr}]] ⟻ [[Journal]] ⟼ [[{mdNextStr}]]"
+
+  # rename all files in the folder
+  # glob and sort the files in alphanumeric order
+  filesToProcess = glob.glob('*')
+  filesToProcess.sort()
+  count = 1
+  for fTP in filesToProcess:
+    if fTP == 'albuminfo':
+      continue
+    
+    fTPname, fTPext = os.path.splitext(fTP)
+
+    newFileName = f"{f}{count:03}{fTPext}"
+    os.system(f"mv {fTP} {newFileName}")
+    count = count + 1
+
+  print(mdFilePath)
+
+  # keep the markdown file open while handling 
+  # all the photo-related operations
+  with open(mdFilePath, 'w') as mdFile:
+
+    # add header to the markdown file
+    mdFile.write(mdFileNav)
+    mdFile.write("\n\n")
+    mdFile.write(mdFileTitle)
+    mdFile.write("\n\n")
+
+    # glob and sort the files in number order
+    photosToProcess = glob.glob('*')
+    photosToProcess.sort()
+
+    # process each photo, skipping 'albuminfo'
+    for p in photosToProcess:
+      if p == 'albuminfo':
+        continue
+
+      pFileName, pFileExtension = os.path.splitext(p)
+
+      # append the photo to the markdown file
+      strToPrint = f"![[{p}|sm]] "
+      mdFile.write(strToPrint)
+
+      # copy the file to the dest_folder
+      strToPrint = f"cp {base_folder}/{f}/{p} {dest_folder}/photos"
+      os.system(strToPrint)
+
+      # create the bibfile entry
+      text = "@graphic{"+f"{pFileName}"+",\n"
+      text = text + "  title = {"+f"{mdTitleStr} #{pFileName[-3:].lstrip('0')}"+"},\n"
+      text = text + "  author = {Hanni, Jenner},\n"
+      text = text + "  date = {" + f"{mdFileDate}" + "}\n"
+      text = text + "  langid = {en},\n"
+      text = text + "  file = {" + f"{dest_folder}/photos/{p}" + "}\n}\n\n"
+      bibfile_entries.append(text)
+
+    # add footer to the markdown file
+    mdFile.write("\n\n")
+    mdFile.write(mdFileNav)
+    mdFile.write("\n\n")
+
+  # be extra super duper sure these vars are reset
+  mdPrevStr = ''
+  mdNextStr = ''
+  mdTitleStr = ''
+  markdown_file = None
+  markdown_file = []
+
+# write out the cumulative bibfile
+with open(temp_folder+'/zotero-import.bib', 'w') as f:
+  for b in bibfile_entries:
+    f.write(b)
+
