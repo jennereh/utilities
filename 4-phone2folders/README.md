fe080c0e5b2dfa16f52ab574fbced42df8a9283e
jennereh <jenner@jennereh.com>
2023-03-03 10:12:44 -0800

    [4-p2f] Processed ~700 files as expected

--- 4-phone2folders/README.md
+++ 4-phone2folders/README.md
@@ -1,6 +1,13 @@
 # phone2folders
 
-Developing process for moving iPhone photos and screenshots to my laptop with Feem, converting the images from HEIC to JPG, sorting them into folders by date. [link](4-phone2folders)
+Automate moving iPhone photos and screenshots to my laptop with Feem, converting any HEIC images to JPG, sorting all imported images into folders by date, and saving any extras in a manual fixes folder for later inspection. [link](4-phone2folders)
+
+## Dependencies
+
+I'm running Linux Mint 20.
+
+1. Files are moved to the trash using `gio` 
+1. `.HEIC` files are converted using `heif-convert`
 
 ## Prep
 
@@ -8,21 +15,29 @@ Developing process for moving iPhone photos and screenshots to my laptop with Fe
 1. Feem's AppImage lives in `~/admin` on the laptop.
 1. Connect the phone and the laptop to the same wifi.
 1. Open the Feem app on both devices.
-1. Transfer all of the files from one to the other. 
+1. Manually click to transfer all of the files from one to the other. 
 1. The transfer often doesn't actually complete, so check the size of the files on the laptop with the total displayed on the hone. If it's wrong, send them all again, Feem will only send the ones that were missed. Do this until all files have been transferred. 
 1. Don't delete anything on the phone until this process is complete.
 
 ## Usage
 
-I should be able to run the script from the terminal with the alias `phone2folders` without having to navigate to any directories.
+I should be able to run this script from the terminal with the alias `phone2folders` without having to navigate to any directories. Added the following to my `.bashrc`.
 
-All files to process should be in `~/` and start with `iPhone_`.
+```
+alias phone2folders="python /home/jenner/projects/jeh-utilities/4-phone2folders/phone2folders.py"
+```
+
+All imported files to process should be in `~/` and start with `iPhone_c`.
+
+As of March 2023, the files seem to be a combination of `.PNG`, `.GIF`, `.JPG`, and `.HEIC`.
 
 This script moves all files to new folders in `~/tosync/photos-to-process` by date.
 
 Each folder should be named in the form `mynameYEARMMDDdaily`.
 
 Each folder should also contain an `albuminfo` text file with a single line `title: Daily`.
+
+The output should be formatted so `zot` can be run immediately.
 
 ## Next Step
 
---
+++ 4-phone2folders/phone2folders.py
@@ -0,0 +1,96 @@
+# phone2folders.py
+# handles .HEIC, .PNG, .JPG, and .GIF 
+# with or without 
+# ---
+# missing exif data goes to 'manual
+
+import os, glob, imghdr
+from PIL import Image
+
+def get_date_taken(path):
+
+  try:
+    exif = Image.open(path)._getexif()
+  except AttributeError as e:
+    return 'FAIL'
+  else:
+    if not exif:
+      return 'FAIL'
+    elif 36867 in exif:
+      return exif[36867]
+    else:
+      return 'FAIL'
+
+fixme_folder = "manualfix"
+desk_folder = "/home/jenner"
+dest_folder = "/home/jenner/tosync/photos-to-process"
+file_prefix = "iPhone_c"
+albuminfo   = "title: Daily"
+
+print("phone2folders.py")
+
+# if the folders do not exist, exit with an error
+# if they do exist, change to the desktop
+if not os.path.isdir(dest_folder):
+  print(f"{dest_folder} does not exist.\nexiting...")
+  input("Press [Enter] to continue.") 
+  exit()
+else: 
+  os.chdir(desk_folder)
+
+# glob the folders in base_folder
+filesList = glob.glob(f"{file_prefix}*", recursive=False)
+
+for f in filesList:
+
+  newFileName = ''
+  fName, fExt = os.path.splitext(f)
+
+  # process files by type
+  # - create JPG from HEIC and put original HEIC in trash
+  # - some iPhone screenshots that have been edited appear to 
+  #   have a suffix of .png but are actually .jpg files (what?!)
+  #   so detect and convert these 
+  # - all other image files with dates get lowercase exts
+  if fExt == '.HEIC':    
+    newFileName = f"{fName}.jpg"
+    os.system(f"heif-convert {f} {newFileName}")
+    os.system(f"gio trash {f}")
+  elif fExt == '.PNG' or fExt == '.png':
+    if imghdr.what(f) == 'jpeg':
+      newFileName = f"{fName}.jpg"
+      os.system(f"mv {f} {newFileName}")
+    else:
+      newFileName = f"{fName}{fExt.lower()}"
+      os.system(f"mv {f} {newFileName}")
+  else:  
+    newFileName = f"{fName}{fExt.lower()}"
+    os.system(f"mv {f} {newFileName}")
+
+  print(f"{newFileName} is ready to be moved")
+
+  # build the new folder's name from the exif data
+  date = get_date_taken(newFileName)
+  if date == 'FAIL':
+    newFolderPath = dest_folder+'/'+fixme_folder
+  else:
+    date = date.split(' ')[0].split(':')
+    yr = date[0]
+    mo = date[1]
+    dy = date[2]
+    newFolderName = f"hanni{yr}{mo}{dy}daily"
+    newFolderPath = dest_folder+'/'+newFolderName
+
+  # make sure the destination folder exists
+  if not os.path.isdir(newFolderPath):
+    os.system(f"mkdir {newFolderPath}")
+
+  # create an albuminfo file if necessary
+  if not os.path.isfile(f"{newFolderPath}/albuminfo"):
+    with open(newFolderPath+'/albuminfo', 'w') as albuminfoFile:
+      albuminfoFile.write(albuminfo)
+
+  # move the file to its dated destination folder
+  os.system(f"mv {newFileName} {newFolderPath}/{newFileName}")
+
+
